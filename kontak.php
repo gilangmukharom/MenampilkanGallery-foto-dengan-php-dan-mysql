2c914403393d829843434574d2a7d2744e023b57
gilangmukharom <55473537+gilangmukharom@users.noreply.github.com>
2020-07-27 23:19:40 +0700

    Add files via upload

---
+++ kontak.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="icon" href="initiket.png">
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
+	<title>Tiketku</title>
+</head>
+<body>
+	<div>
+		<img class="header" src="../css/initiket.png">
+		<h1 class="header">Gallery Tugas</h1>
+	</div>
+		<div class="footer">
+			<p>By: Gilang Mukharom</p>
+		</div>
+		<div class="menu">
+			<ul class="menu">
+				<li class="menu"><a class="menu" href="index.php">Home</a></li>
+				<li id="active" class="menu"><a class="menu" href="kontak.php.html">Contact</a></li>
+			</ul>
+		</div>
+			<div>
+				<h3>Ini kontak saya bila anda ingin menghubungi</h3>
+				<a href="https://www.instagram.com//p/B7f-usdHWVP/?igshid=1vya9h7n1y3gj">
+		<button type="button" name="instagram">
+						instagram : (gilang mukharom)
+					</button>
+				<div>
+					<a href="https://www.Twitter.com/Gilaaaaaangin/s=09">
+					<button type="button" name="Twitter">
+						Twitter : @Gilaaaaangin
+					</button>
+	</div>
+	</div>
+	</div>
+</body>
+</html>
